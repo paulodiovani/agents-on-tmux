f53e7d9179dad03a491cb1b468c030a5ed9c2d93
Paulo Diovani Gonçalves <1295197+paulodiovani@users.noreply.github.com>
2026-07-04 14:58:35 -0300

    Wire frontend

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,13 @@
-#[allow(dead_code)]
 mod backends;
+mod frontends;
 
-fn main() {
-    println!("Hello, world!");
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    use backends::Tmux;
+    let driver = backends::TmuxDriver;
+    driver.create_session_if_not_exists()?;
+    let terminal = ratatui::init();
+    let mut app = frontends::App::new(&driver)?;
+    app.run(terminal, &driver)?;
+    ratatui::restore();
+    Ok(())
 }
